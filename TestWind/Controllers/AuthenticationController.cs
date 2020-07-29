daae46dad938bebf2886d810d148cc95c945abf0
roy m <roym.iphone@gmail.com>
2020-07-29 22:46:24 +0300

    New repo

---
+++ TestWind/Controllers/AuthenticationController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TestWind.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : Controller
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+        private readonly IConfiguration _config;
+        private List<User> appUsers = new List<User> {
+            new User { FullName = "Roy Mandel", UserName = "admin", Password = "1234", UserRole = "Admin" },
+            new User { FullName = "Noam Mandel", UserName = "noam", Password = "2159", UserRole = "User" },
+            new User { FullName = "Matan Mandel", UserName = "matan", Password = "2121", UserRole = "User" }
+        };
+
+        public AuthenticationController(ILogger<AuthenticationController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] User login)
+        {
+            IActionResult response = Unauthorized();
+
+            User user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJWTToken(user);
+                response = Ok(new
+                {
+                    token = tokenString,
+                    userDetails = user
+                });
+            }
+            return response;
+        }
+        User AuthenticateUser(User loginCredentials)
+        {
+            return appUsers.SingleOrDefault(s => s.UserName == loginCredentials.UserName && s.Password == loginCredentials.Password);
+        }
+
+        string GenerateJWTToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:SecretKey"]));
+
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+
+            var claims = new[]
+            {
+                    new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                    new Claim("fullName", userInfo.FullName),
+                    new Claim("role", userInfo.UserRole),
+                    new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                };
+
+            var token = new JwtSecurityToken(
+               issuer: _config["Jwt:Issuer"],
+               audience: _config["Jwt:Audience"],
+               claims: claims,
+               expires: DateTime.Now.AddMinutes(30),
+               signingCredentials: credentials
+             );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+}
---
+++ TestWind/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWind.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        [Route("GetUser")]
+        [Authorize(Policy = Policies.User)]
+        public IActionResult GetUserData()
+        {
+            return Ok("This is a response from user method");
+        }
+
+        [HttpGet]
+        [Route("GetAdmin")]
+        [Authorize(Policy = Policies.Admin)]
+        public IActionResult GetAdminData()
+        {
+            return Ok("This is aresponse from admin method");
+        }
+    }
+}
